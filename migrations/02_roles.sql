69a2928b3ab1c32d35efe7f570c0f23adf5e1a3d
Ilyavosky <ilyacortesruiz@gmail.com>
2026-04-18 17:00:37 -0600

    Implementar procedures y modificar permisos de los roles

--- migrations/02_roles.sql
+++ migrations/02_roles.sql
@@ -21,11 +21,13 @@ GRANT INSERT ON citas, vacunas_aplicadas TO rol_veterinario;
 GRANT USAGE ON SEQUENCE citas_id_seq, vacunas_aplicadas_id_seq TO rol_veterinario;
 GRANT SELECT ON vacunas_aplicadas, inventario_vacunas TO rol_veterinario;
 GRANT EXECUTE ON PROCEDURE sp_agendar_cita(INT, INT, TIMESTAMP, TEXT, INT) TO rol_veterinario;
+GRANT EXECUTE ON PROCEDURE sp_registrar_mascota(VARCHAR, VARCHAR, DATE, INT, INT) TO rol_veterinario;
 GRANT EXECUTE ON FUNCTION fn_total_facturado(INT, INT) TO rol_veterinario;
 GRANT SELECT ON v_mascotas_vacunacion_pendiente TO rol_veterinario;
 
 GRANT SELECT ON mascotas, duenos, citas, veterinarios TO rol_recepcionista;
-GRANT INSERT ON citas TO rol_recepcionista;
-GRANT USAGE ON SEQUENCE citas_id_seq TO rol_recepcionista;
+GRANT INSERT ON citas, mascotas TO rol_recepcionista;
+GRANT USAGE ON SEQUENCE citas_id_seq, mascotas_id_seq TO rol_recepcionista;
 GRANT EXECUTE ON PROCEDURE sp_agendar_cita(INT, INT, TIMESTAMP, TEXT, INT) TO rol_recepcionista;
+GRANT EXECUTE ON PROCEDURE sp_registrar_mascota(VARCHAR, VARCHAR, DATE, INT, INT) TO rol_recepcionista;
 GRANT SELECT ON v_mascotas_vacunacion_pendiente TO rol_recepcionista;
